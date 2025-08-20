ed57dfda735ac4d88a890abdfbf4bfb13695d358
Riyad Seriai <blackomegadu42@gmail.com>
2025-08-20 02:56:08 +0200

    Patch module for webhook 2

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { GameService } from './game.services';
 import { GameController } from './game.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { DiscordWebhookService } from 'src/discord/discord-webhook.service';
 
 @Module({
     imports: [PrismaModule],
-    providers: [GameService],
+    providers: [GameService, DiscordWebhookService],
     controllers: [GameController],
 })
 export class GameModule { }
